7b900473e1c33187e61a47b22288889441711052
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-01-02 01:19:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
+import { Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <h1> Meme Generator </h1>
-    <h2>Meme we laugh at </h2>
+       <Routes>
+        <Route path='/'element={<HomePage/>} />
+       </Routes>
     </div>
   );
 }
