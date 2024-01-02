2393031c856016047b81671f382faf3e4d285162
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-01-03 03:22:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from './pages/Home';
+import EditPage from './pages/Edit';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
        <h1> Meme Generator </h1>
        <Routes>
         <Route path='/'element={<HomePage/>} />
+        <Route path='/edit'element={<EditPage/>} />
        </Routes>
     </div>
   );
