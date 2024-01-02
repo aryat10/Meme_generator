8a3b7ec8a9c202e8c5e1a925e6aa8170d14d4aa3
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-01-03 03:25:00 +0530

    Create text.jsx

---
+++ src/components/text.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+import Draggable from "react-draggable";
+const TextPage = () =>{
+    const [editMode,setEditMode] = useState(false);
+    
+    const [val,setVal] = useState("Double Click to Edit your meme!!");
+   
+    return(
+     <Draggable>
+        {
+            editMode ? (<input onDoubleClick={(e)=> setEditMode(false)} 
+            value={val} 
+            onChange={(e) => setVal(e.target.value)}  />
+            ) : (
+             <h1 onDoubleClick={e=> setEditMode(true)}>{val}</h1>
+            )}
+     </Draggable>   
+    )
+}
+
+export default TextPage;
